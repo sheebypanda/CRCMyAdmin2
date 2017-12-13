6a31e9517e19bbe79e4e9b965bf6efcaeb1c7270
sheebypanda <sheebypanda@gmail.com>
2017-12-13 13:23:32 +0100

    deploying day 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "CRCMyAdmin2"
 set :branch, :master
 set :pty, true
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :bundle_binstubs, nil
