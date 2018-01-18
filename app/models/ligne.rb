0a39d3af7d89fc0487a5da1882b723a3260309df
sheebypanda <sheebypanda@gmail.com>
2018-01-18 17:00:24 +0100

    add control and validation to models

--- app/models/ligne.rb
+++ app/models/ligne.rb
@@ -1,6 +1,6 @@
 class Ligne < ApplicationRecord
   belongs_to :operateur
   has_many :recettes
-  #validates :numerocompte, presence: true, uniqueness: true
+  validates :numerocompte, presence: true, uniqueness: true
 
 end
--- app/models/localisation.rb
+++ app/models/localisation.rb
@@ -4,18 +4,18 @@ class Localisation < ApplicationRecord
   has_many :lignes, through: :recettes
 
   validates :nom, presence: true
-  #
-  #
-  # geocoded_by :adresse
-  # after_validation :geocode, :if => lambda{ |obj| obj.adresse_changed? }
-  #
-  # reverse_geocoded_by :lat, :lng, :address => :adresse do |obj,results|
-  #   if geo = results.first
-  #     obj.adresse = geo.street_number+" "+geo.route
-  #     obj.ville = geo.city
-  #     obj.codepostal = geo.postal_code
-  #   end
-  # end
-  #
-  # after_validation :reverse_geocode, :if => lambda{ |obj| obj.lat_changed? }
+  
+
+  geocoded_by :adresse
+  after_validation :geocode, :if => lambda{ |obj| obj.adresse_changed? }
+
+  reverse_geocoded_by :lat, :lng, :address => :adresse do |obj,results|
+    if geo = results.first
+      obj.adresse = geo.street_number+" "+geo.route
+      obj.ville = geo.city
+      obj.codepostal = geo.postal_code
+    end
+  end
+
+  after_validation :reverse_geocode, :if => lambda{ |obj| obj.lat_changed? }
 end
