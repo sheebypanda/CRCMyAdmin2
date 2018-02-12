41e4666d02aba6594d58dbb6e9d8702725599306
sheebypanda <sheebypanda@gmail.com>
2018-02-12 14:23:24 +0100

    linking objects properly

--- app/models/equipement.rb
+++ app/models/equipement.rb
@@ -1,4 +1,6 @@
 class Equipement < ApplicationRecord
   has_one :recette, dependent: :destroy
+  has_one :localisation, through: :recette
+  has_one :ligne, through: :recette
   validates :serial, presence: true
 end
--- app/models/ligne.rb
+++ app/models/ligne.rb
@@ -1,6 +1,8 @@
 class Ligne < ApplicationRecord
   belongs_to :operateur
   has_many :recettes
+  has_many :equipements, through: :recettes
+  has_one :localisation, through: :recettes
   validates :numerocompte, presence: true, uniqueness: true
 
 end
