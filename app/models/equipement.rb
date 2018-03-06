4e11ddaaa9b6e84b9bb9432b93fcd816fad029d2
sheebypanda <sheebypanda@gmail.com>
2018-03-06 15:15:43 +0100

    commenting serial control, and make searching equipement throught ip working

--- app/models/equipement.rb
+++ app/models/equipement.rb
@@ -2,14 +2,18 @@ class Equipement < ApplicationRecord
   has_one :recette, dependent: :destroy
   has_one :localisation, through: :recette
   has_one :ligne, through: :recette
-  validates :serial, presence: true
+  # validates :serial, presence: true
 
   include PgSearch
-  pg_search_scope :equipement_search, :against => {
-    :nom => 'A',
-    :ip => 'B',
-    :asapid => 'C',
-    :modele => 'D'
-  }
+  pg_search_scope :equipement_search, :against =>
+    {
+      :ip => 'A',
+      :nom => 'B',
+      :asapid => 'C',
+      :modele => 'D'
+    },
+    using: {
+            tsearch: { prefix: true, any_word: true }
+        }
 
 end
