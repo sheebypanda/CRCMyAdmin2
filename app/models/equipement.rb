bd6fb0afa3d326104227b8bd282070dff54af537
sheebypanda <sheebypanda@gmail.com>
2018-04-13 14:54:08 +0200

    add localisation_id to equipement export and import

--- app/models/equipement.rb
+++ app/models/equipement.rb
@@ -28,10 +28,10 @@ class Equipement < ApplicationRecord
       equipement_hash = row.to_hash
 
       #Check si equipement existe par le SN ; Sinon on le créé
-      unless equipement_hash["serial"].to_s.empty?
+      if equipement_hash["serial"].present?
         equipement = Equipement.where(serial: equipement_hash["serial"])
         equipement_hash["datemaintenance"] = Date.parse(equipement_hash["datemaintenance"]) if equipement_hash["datemaintenance"]
-        equipement_hash = equipement_hash.except!('localisation', 'adresse', 'codepostal', 'ville', 'etage', 'tel', 'mail', 'description', 'lat', 'lng', 'horaires')
+        equipement_hash = equipement_hash.except!('id_localisation', 'localisation', 'adresse', 'codepostal', 'ville', 'etage', 'tel', 'mail', 'description', 'lat', 'lng', 'horaires')
         if equipement.count == 1
           equipement.first.update_attributes(equipement_hash)
         elsif equipement.count == 0
@@ -40,36 +40,55 @@ class Equipement < ApplicationRecord
       end
 
       equipement_hash = row.to_hash
-      #Check si localisation existe par son nom & ville ; Sinon on la créée
-      unless equipement_hash["localisation"].to_s.empty?
-        localisation = Localisation.where(nom: equipement_hash["localisation"], ville: equipement_hash['ville'])
-        equipement_hash = equipement_hash.except!('marque', 'modele', 'nom', 'serial', 'asapid', 'ip', 'iosv', 'sla', 'maintenance', 'coutmaintenance', 'datemaintenance')
-        equipement_hash["nom"] = equipement_hash.delete("localisation")
-        if localisation.count == 1
-          localisation.first.update_attributes(equipement_hash)
-        elsif localisation.count == 0
-          Localisation.create!(equipement_hash)
+      #Check si localisation existe par son id, sinon par son nom & ville, sinon on la créée
+      if equipement_hash["id_localisation"].present?
+        begin
+          localisation = Localisation.find(equipement_hash["id_localisation"])
+        rescue
+          localisation = nil
         end
+      elsif equipement_hash["localisation"].present?
+        localisations = Localisation.where(nom: equipement_hash["localisation"], ville: equipement_hash['ville'])
+      end
+      equipement_hash = equipement_hash.except!('marque', 'modele', 'nom', 'serial', 'asapid', 'ip', 'iosv', 'sla', 'maintenance', 'coutmaintenance', 'datemaintenance')
+      equipement_hash["nom"] = equipement_hash.delete("localisation")
+      equipement_hash["id"] = equipement_hash.delete("id_localisation")
+      if localisation.present?
+        localisation.update_attributes(equipement_hash)
+      elsif localisations.present?
+        equipement_hash = equipement_hash.except!('id')
+        localisations.first.update_attributes(equipement_hash)
+      elsif equipement_hash['nom'].present? and equipement_hash['ville'].present? and equipement_hash['adresse'].present?
+        Localisation.create!(equipement_hash)
       end
 
       equipement_hash = row.to_hash
       # Check si une recette lie la localisation à l'equipement. Sinon, on la créée
-      unless equipement_hash["serial"].to_s.empty? or equipement_hash["localisation"].to_s.empty?
+      if equipement_hash["serial"].present? and (equipement_hash["id_localisation"].present? or (equipement_hash["localisation"].present? and equipement_hash['ville'].present?))
+        recette_hash = Hash.new
         recette = Recette.joins(:equipement).where('equipements.serial' => equipement_hash["serial"])
         equipement = Equipement.where(serial: equipement_hash["serial"])
-        localisation = Localisation.where(nom: equipement_hash["localisation"], ville: equipement_hash['ville'])
+        if equipement_hash['id_localisation'].present?
+          begin
+            localisation = Localisation.find(equipement_hash['id_localisation'])
+            recette_hash['localisation_id'] = localisation.id
+          rescue
+            localisation = nil
+          end
+        else
+          localisations = Localisation.where(nom: equipement_hash["localisation"], ville: equipement_hash['ville'])
+          recette_hash['localisation_id'] = localisations.ids.first
+        end
         # equipement_hash = equipement_hash.except!('marque', 'modele', 'nom', 'serial', 'asapid', 'ip', 'iosv', 'sla', 'maintenance', 'coutmaintenance', 'datemaintenance','localisation', 'adresse', 'codepostal', 'ville', 'etage', 'tel', 'mail', 'description', 'lat', 'lng', 'horaires')
-        recette_hash = Hash.new
         recette_hash['user_id'] = 1
         recette_hash['equipement_id'] = equipement.ids.first
-        recette_hash['localisation_id'] = localisation.ids.first
         recette_hash['ligne_id'] = fibreBM.ids.first
         recette_hash['supervision'] = true
         recette_hash['snmp'] = true
         recette_hash['etiquette'] = true
         if recette.count == 1
           recette.first.update_attributes(recette_hash)
-        elsif recette.count == 0
+        elsif recette.count == 0 and recette_hash['localisation_id'].present?
           Recette.create!(recette_hash)
         end
       end
