2c6239f8545dcfdee520e164d14200431e9ba844
sheebypanda <sheebypanda@gmail.com>
2018-04-27 15:11:30 +0200

    add geoloc proxy

--- app/models/localisation.rb
+++ app/models/localisation.rb
@@ -17,19 +17,19 @@ class Localisation < ApplicationRecord
       any_word: true
     }
   }
-  #
-  # geocoded_by :adresse
-  # after_validation :geocode, :if => lambda{ |obj| obj.adresse_changed? }
-  #
-  # reverse_geocoded_by :lat, :lng, :address => :adresse do |obj,results|
-  #   if geo = results.first
-  #     obj.adresse = geo.street_number+" "+geo.route
-  #     obj.ville = geo.city
-  #     obj.codepostal = geo.postal_code
-  #   end
-  # end
-  #
-  # after_validation :reverse_geocode, :if => lambda{ |obj| obj.lat_changed? }
+
+  geocoded_by :adresse
+  after_validation :geocode, :if => lambda{ |obj| obj.adresse_changed? }
+
+  reverse_geocoded_by :lat, :lng, :address => :adresse do |obj,results|
+    if geo = results.first
+      obj.adresse = geo.street_number+" "+geo.route
+      obj.ville = geo.city
+      obj.codepostal = geo.postal_code
+    end
+  end
+
+  after_validation :reverse_geocode, :if => lambda{ |obj| obj.lat_changed? }
 
   require 'csv'
 
---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,3 @@
+Geocoder.configure(
+  :http_proxy => 'http://172.20.92.38:3128'
+)
