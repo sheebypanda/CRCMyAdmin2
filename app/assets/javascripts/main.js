6427c9316b29d2c5398dd0ee5c6cc63ff9855210
sheebypanda <sheebypanda@gmail.com>
2018-05-25 15:30:09 +0200

    add filter to edit recette

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -61,12 +61,11 @@ document.addEventListener("turbolinks:load", function() {
 
   $("#tablesorter").tablesorter();
 
-  $("#recette_localisation_id").hide();
-  $(".internet").hide();
+  $(".recette_localisation_id").hide();
 
   $("#recette_localisation_ville").change(function() {
     var ville = $( this ).val();
-    $("#recette_localisation_id").show();
+    $(".recette_localisation_id").show();
     $(".loc").hide();
     var option = "."
     option += ville.replace(/ /g,'')
@@ -74,12 +73,18 @@ document.addEventListener("turbolinks:load", function() {
   });
 
   $("#recette_localisation_id").change(function(){
-    $(".internet").show();
     var localisation_id = $(this).val();
     $(".ligne").hide();
     var ligne_option = ".";
     ligne_option += localisation_id;
     $(ligne_option).show();
+  });
+
+  $(".text-filtre").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#filtre-select-loc select option").filter(function(){
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
   });
 
 })
