337dc2309b3d2abb759921efd2ec061f52983593
sheebypanda <sheebypanda@gmail.com>
2018-10-29 15:32:48 +0100

    add storage to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, "CRCMyAdmin2"
 set :branch, :master
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads storage}
 set :keep_releases, 5
 set :bundle_binstubs, nil
 set :rbenv_type, :user
