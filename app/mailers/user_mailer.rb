e8ada87a581948505d5b12ca6643d17eb545245e
sheebypanda <sheebypanda@gmail.com>
2019-01-03 11:52:52 +0100

    add nxo to emailing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,18 +7,21 @@ class UserMailer < ApplicationMailer
     email_nxo = Rails.application.secrets.email_nxo
     email_spie = Rails.application.secrets.email_spie
 
-    email_copy1 = Rails.application.secrets.email_copy1
-    email_copy2 = Rails.application.secrets.email_copy2
-    email_copy3 = Rails.application.secrets.email_copy3
-    email_copy4 = Rails.application.secrets.email_copy4
-    email_copy5 = Rails.application.secrets.email_copy5
+    email_copy1 = Rails.application.secrets.email_copy1 #AC
+    email_copy2 = Rails.application.secrets.email_copy2 #CP
+    email_copy3 = Rails.application.secrets.email_copy3 #CC
+    email_copy4 = Rails.application.secrets.email_copy4 #EN
 
     destination = []
 
+    destination << Rails.application.secrets.email_from
+    destination << email_nxo
     destination << email_copy1
-    # destination << email_copy2
-  #  destination << Rails.application.secrets.email_from
+    destination << email_copy2
+    destination << email_copy3
+    destination << email_copy4
 
     mail(to: destination, subject: "Bordeaux Métropole - Déclaration d'incident sur équipements réseaux" )
+    
   end
 end
