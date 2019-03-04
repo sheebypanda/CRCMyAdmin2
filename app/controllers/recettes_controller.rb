b911b269502b2094b0ccacf465565686c21b1e87
sheebypanda <sheebypanda@gmail.com>
2019-03-04 15:21:28 +0100

    delete aerohives

--- app/controllers/recettes_controller.rb
+++ app/controllers/recettes_controller.rb
@@ -28,7 +28,7 @@ class RecettesController < ApplicationController
   end
 
   def dashboard
-    @eqs = Equipement.select(:marque, :modele).where.not(marque: [nil, '', 'net-snmp', 'Unknown', 'Moxa Technologies Co., Ltd.', 'PipingHot Networks Limited'], 'equipements.nom' => 'Cimetière Chartreuse-Loge').distinct(:modele)
+    @eqs = Equipement.select(:marque, :modele).where.not(marque: [nil, '', 'net-snmp', 'Unknown', 'Moxa Technologies Co., Ltd.', 'PipingHot Networks Limited', 'Aerohive'], 'equipements.nom' => 'Cimetière Chartreuse-Loge').distinct(:modele)
   end
 
   def map
