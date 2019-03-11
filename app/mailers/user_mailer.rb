a5f79a53e786ac019eaefa9453d757c24c539254
sheebypanda <sheebypanda@gmail.com>
2019-03-11 13:27:45 +0100

    add email recipitent

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,17 +9,17 @@ class UserMailer < ApplicationMailer
 
     email_copy1 = Rails.application.secrets.email_copy1 #AC
     email_copy2 = Rails.application.secrets.email_copy2 #CP
-    email_copy3 = Rails.application.secrets.email_copy3 #CC
+    email_copy3 = Rails.application.secrets.email_copy3 #FJ
     email_copy4 = Rails.application.secrets.email_copy4 #EN
 
     destination = []
 
-#    destination << Rails.application.secrets.email_from
-#    destination << email_nxo
+    destination << Rails.application.secrets.email_from
+    destination << email_nxo
     destination << email_copy1
-#    destination << email_copy2
- #   destination << email_copy3
- #   destination << email_copy4
+    destination << email_copy2
+    destination << email_copy3
+    destination << email_copy4
 
     mail(to: destination, subject: "Bordeaux Métropole - Déclaration d'incident sur équipements réseaux" )
 
