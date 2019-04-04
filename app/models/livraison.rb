aa54fe0c36efc646f617e001dc6ad047eddbda68
sheebypanda <sheebypanda@gmail.com>
2019-04-04 14:43:08 +0200

    add commande bm required for new livraison

--- app/models/livraison.rb
+++ app/models/livraison.rb
@@ -2,5 +2,6 @@ class Livraison < ApplicationRecord
   belongs_to :user
   has_many :equipements
   has_many_attached :pvs
-  validates :nom, uniqueness: true
+  validates :nom, presence: true, uniqueness: true
+  validates :commande, presence: true
 end
